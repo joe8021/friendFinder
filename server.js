462dc38abd7c84abd9ae7cc02a5e91a545b2c6ff
joe8021 <vanhornjoe8@gmail.com>
2019-10-15 14:51:25 -0500

    heroku inital

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
 
+var express = require("express");
+
+
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+//require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
